9f79cf5a78c316c8107b37096c0e6fb968421657
vsinghal3737 <42525531+vsinghal3737@users.noreply.github.com>
2020-03-22 18:53:27 -0700

    Initial Phase

---
+++ HawkDove/HawkDove/Assets/Scripts/Temp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject prefab = Resources.Load("HawksSphere") as GameObject;
+        for (int i=100; i < 102; i++)
+        {
+            GameObject go = Instantiate(prefab) as GameObject;
+            go.transform.position = new Vector3(i*5, i, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
